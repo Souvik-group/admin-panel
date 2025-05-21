c4b86aadc893a8fe730aec386c8feb0d8f18660d
Souvik-group <nanupatra855@gmail.com>
2025-05-21 20:05:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,6 +238,9 @@ def logout():
     session.clear()
     return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    from os import environ
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
